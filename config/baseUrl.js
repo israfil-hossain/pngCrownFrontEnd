79d7a41925efb9bd73f538045b8bbd6307ea322d
Israfil-Hossain <Israfil166091@gmail.com>
2023-04-15 21:11:02 +0600

    update BaseUrl

--- config/baseUrl.js
+++ config/baseUrl.js
@@ -1,4 +1,4 @@
 export const baseUrl = {
-    dev: "http://localhost:8000/api",
-    // dev: "http://89.116.228.42:8000/api",
+    // dev: "http://localhost:8000/api",
+    dev: "http://89.116.228.42:8000/api",
   };
